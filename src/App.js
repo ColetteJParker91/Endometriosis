59bda19a8c57291a12f7e52a9b19b21be7205a43
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-10-26 17:41:13 +0200

    Initial commit
    Components created.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Symptoms />
+      <Treatment />
+      <Surgery />
+      <Pain />
+      <Specialist />
+      <Info />
+      <Research />
+      <Support />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
