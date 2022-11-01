0862cd24ce6965de79402b098adf01da1cf73322
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-11-01 19:38:52 +0100

    css update
    font added

---
+++ src/components/symptoms/Symptoms.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./symptoms.css";
+
+const Symptoms = () => {
+  return (
+    <section id="symptoms">
+      <h2>Symptoms of Endometriosis</h2>
+      <div className="container symptoms-container"></div>
+    </section>
+  );
+};
+
+export default Symptoms;
