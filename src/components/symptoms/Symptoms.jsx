1aa0a906dbbe23876634b782058c8376283a4859
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-11-02 15:08:42 +0100

    All general symptoms data added

--- src/components/symptoms/Symptoms.jsx
+++ src/components/symptoms/Symptoms.jsx
@@ -1,17 +1,62 @@
 import React from "react";
 import "./symptoms.css";
+import { VscDebugBreakpointLog } from "react-icons/vsc";
 
 const Symptoms = () => {
   return (
     <section id="symptoms">
       <h2>Symptoms of Endometriosis</h2>
       <div className="container symptoms-container">
-        <article className="symptoms-one">
-            <ul className="symptons-one-list">
-                <li>
-                    <p>Pelvic, abdominal or lower back pain that can worsen during your period</p>
-                </li>
-            </ul>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>
+            Pelvic, abdominal or lower back pain that can worsen during your
+            period
+          </h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>
+            Pain during your period that effects your ability to do normal
+            activities
+          </h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Pain during or after sexual intercourse</h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Difficulty getting pregnant</h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Pain during bowel movements</h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>
+            Nausea, bloating, constipation, diarrhoea or blood in your urine or bowel
+            movements during your period
+          </h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>
+            Menorrhagia - excessive or prolonged bleeding during your period
+          </h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Spotting or bleeding between menstural periods</h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Fatigue or low energy</h3>
+        </article>
+        <article className="symptoms_details">
+          <VscDebugBreakpointLog className="symptoms_details-icon" />
+          <h3>Loss of dark blood or clots before your period</h3>
         </article>
       </div>
     </section>
