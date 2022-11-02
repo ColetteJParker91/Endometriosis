663d739075548445102910f9725aa9fdd6b41691
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-11-02 14:02:10 +0100

    Work on symptoms section

--- src/components/symptoms/Symptoms.jsx
+++ src/components/symptoms/Symptoms.jsx
@@ -5,7 +5,15 @@ const Symptoms = () => {
   return (
     <section id="symptoms">
       <h2>Symptoms of Endometriosis</h2>
-      <div className="container symptoms-container"></div>
+      <div className="container symptoms-container">
+        <article className="symptoms-one">
+            <ul className="symptons-one-list">
+                <li>
+                    <p>Pelvic, abdominal or lower back pain that can worsen during your period</p>
+                </li>
+            </ul>
+        </article>
+      </div>
     </section>
   );
 };
